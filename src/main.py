71bc3620bd8534206b6cdb06a4f98866f4cf04e4
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2025-01-03 19:03:56 +0100

    Add and configure linting tools

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-import uvicorn
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI(title="My FastAPI Project")
 
 
 @app.get("/health")
-def read_root():
+def read_root() -> str:
     return "OK"
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # nosec

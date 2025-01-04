256e2e6f437318c40bc52203043fbbdd7d52b9fb
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2025-01-04 16:34:02 +0100

    Create desirect folders structure. Add dependencies. Create base and project tables.

---
+++ src/adapters/database/tables/base.py
@@ -0,0 +1,19 @@
+from sqlalchemy import MetaData, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+naming_convention = {
+    "ix": "ix_%(table_name)s_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=naming_convention)
+
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=True, onupdate=func.now())
---
+++ src/adapters/database/tables/project.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Date, String, Text
+from sqlalchemy.orm import mapped_column, Mapped
+
+from src.adapters.database.tables.base import Base
+from geoalchemy2 import Geometry
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(32), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    start_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    end_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    area_of_interest: Mapped[str] = mapped_column(Geometry("MULTIPOLYGON"), nullable=False)

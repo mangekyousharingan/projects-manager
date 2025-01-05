5c79d4b37df98d9e435831d7fcebaff13cdcaeb3
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2025-01-05 14:52:08 +0100

    Add lifespan with DB check

--- src/main.py
+++ src/main.py
@@ -1,6 +1,42 @@
-from fastapi import FastAPI
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+import logging
+import os
 
-app = FastAPI(title="My FastAPI Project")
+from fastapi import FastAPI
+from psycopg2 import OperationalError
+from sqlalchemy import create_engine, text
+
+# TODO: Added temporary testing
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# TODO: Added temporary for testing
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+engine = create_engine(DATABASE_URL)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
+    logger.info("Starting lifespan...")
+    try:
+        with engine.connect() as conn:
+            logger.info("Testing database connection...")
+            conn.execute(text("SELECT 1"))
+        logger.info("Database connection successful!")
+    except OperationalError as e:
+        logger.info(f"Database connection failed: {str(e)}")
+        raise RuntimeError("Failed to connect to the database") from e
+
+    yield
+
+    logger.info("Application is shutting down...")
+
+
+app = FastAPI(title="Projects manager API", lifespan=lifespan)
 
 
 @app.get("/health")
